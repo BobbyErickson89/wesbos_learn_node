ec8ef23305575eebe448da92a73c1f71a69a4a02
Bobby Erickson <ericksonr07@gmail.com>
2019-07-07 20:57:43 -0400

    - Added a flash notification to createStore controller. - Re-factored our store variable in createStore.  Did this to give it access to the slug key.

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    res.render('index');
+    res.render('index'); 
 };
 
 exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
-}
+};
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save();
-    console.log('it worked');
-    res.redirect('/');
-}
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully created ${store.name}.  Care to leave a review?`); // we get flash from our middleware in app.js 
+    res.redirect(`/store/${store.slug}`);
+};
